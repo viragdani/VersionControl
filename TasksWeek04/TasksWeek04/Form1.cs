8b4f9e1963e070171aa81e965271e51fce4c7018
Dániel Virág <dani.virag99@gmail.com>
2020-10-04 11:21:16 +0200

    Adatok betöltése

--- TasksWeek04/TasksWeek04/Form1.cs
+++ TasksWeek04/TasksWeek04/Form1.cs
@@ -10,11 +10,19 @@ using System.Windows.Forms;
 
 namespace TasksWeek04
 {
+    
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
