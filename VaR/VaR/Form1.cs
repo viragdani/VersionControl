3dfee6a9017d3765e01153c5b83fa3d8344ecaa6
Dániel Virág <dani.virag99@gmail.com>
2020-10-10 21:58:04 +0200

    after#7

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -31,6 +31,20 @@ namespace VaR
 
             dataGridView2.DataSource = Portfolio;
         }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 
 }
