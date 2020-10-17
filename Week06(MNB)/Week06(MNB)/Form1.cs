03a8d046a6be9d3644315054eb0a5b7258417350
Dániel Virág <dani.virag99@gmail.com>
2020-10-17 23:43:30 +0200

    after#3

--- Week06(MNB)/Week06(MNB)/Form1.cs
+++ Week06(MNB)/Week06(MNB)/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week06_MNB_.MnbServiceReference;
 
 namespace Week06_MNB_
 {
@@ -15,6 +16,19 @@ namespace Week06_MNB_
         public Form1()
         {
             InitializeComponent();
+            CallWebservice();
         }
+
+        private void CallWebservice() 
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+            var response=mnbService.GetExchangeRates(request);
+            var result=response.GetExchangeRatesResult;
+        }
+        
     }
 }
