4698a9f22e43072eab12e8cc458ea8e8caa87d58
Dániel Virág <dani.virag99@gmail.com>
2020-10-18 01:02:41 +0200

    Chart

--- Week06(MNB)/Week06(MNB)/Form1.cs
+++ Week06(MNB)/Week06(MNB)/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using Week06_MNB_.Entities;
 using Week06_MNB_.MnbServiceReference;
@@ -23,6 +24,21 @@ namespace Week06_MNB_
             CallWebservice();
             dataGridView1.DataSource = Rates;
             XmlProcessing();
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void CallWebservice() 
