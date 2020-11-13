c66f2460158ca4abcb6b42687b5aff9cb5b5da57
Dániel Virág <dani.virag99@gmail.com>
2020-11-13 23:02:11 +0100

    7Car

---
+++ Futoszalag 8.het/Futoszalag 8.het/Entitites/Car.cs
@@ -0,0 +1,19 @@
+﻿using Futoszalag_8.het.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futoszalag_8.het.Entitites
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
