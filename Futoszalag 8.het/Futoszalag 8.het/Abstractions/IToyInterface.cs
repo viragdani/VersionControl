785b6d309772ac57e299de5da45fcd1589a21688
Dániel Virág <dani.virag99@gmail.com>
2020-11-14 00:15:38 +0100

    8FactoryAbstraction

---
+++ Futoszalag 8.het/Futoszalag 8.het/Abstractions/IToyInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futoszalag_8.het.Abstractions
+{
+    public interface IToyInterface
+    {
+        Toy CreateNew();
+    }
+}
