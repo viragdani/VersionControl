dfad8690075e9d9c6b476dd36c17473c8950ab6f
Dániel Virág <dani.virag99@gmail.com>
2020-11-22 15:22:22 +0100

    After #10

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -48,6 +49,30 @@ namespace UnitTestExample.Test
             Assert.AreEqual(email, actualResult.Email);
             Assert.AreEqual(password, actualResult.Password);
             Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+        [
+    Test,
+    TestCase("irf@uni-corvinus", "Abcd1234"),
+    TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+    TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+    TestCase("irf@uni-corvinus.hu", "Ab1234"),
+]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            //Arrange
+            try
+            {
+                var accountController = new AccountController();
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+              
         }
     }
 }
