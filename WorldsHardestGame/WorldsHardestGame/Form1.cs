d82a2d7c9a7ec87128e7c788eeab4a1a0cce0b7c
Dániel Virág <dani.virag99@gmail.com>
2020-11-28 19:29:26 +0100

    Initialize

---
+++ WorldsHardestGame/WorldsHardestGame/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorldsHardestGame
+{
+    public partial class Form1 : Form
+    {
+        GameController gc = new GameController();
+        GameArea ga;
+        public Form1()
+        {
+            InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+        }
+    }
+}
