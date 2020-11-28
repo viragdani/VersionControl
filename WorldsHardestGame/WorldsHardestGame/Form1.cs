530fcf70eccde518e0f71ccc76beef57dff63663
Dániel Virág <dani.virag99@gmail.com>
2020-11-28 18:53:26 +0100

    GameArea

--- WorldsHardestGame/WorldsHardestGame/Form1.cs
+++ WorldsHardestGame/WorldsHardestGame/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace WorldsHardestGame
 {
+    
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
         public Form1()
         {
             InitializeComponent();
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
         }
     }
 }
